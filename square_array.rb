70d369c9d6420dc3eda68e002792040a7e286c4e
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-25 20:09:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  square_array = [1, 2, 3]
-  square_array.each do |num|
-    square_array << num **2 
+  array = [1, 2, 3]
+  array.each do |num|
+    array << num **2 
   end
   square_array
 end
